854f35182df780fe14aa76b584a20a7f3ccb7a96
Wowsaruss <russell.hayes.dev@gmail.com>
2019-04-17 17:11:57 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+import { VictoryBar, VictoryChart } from 'victory';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      transactionList: [{ type: 1, amount: 13000 }]
+    };
+  }
+  componentDidMount() {
+    axios.get(`${process.env.TRANSACTIONS_API}/transactions`).then(res => {
+      this.setState({
+        transactionList: res.data
+      });
+    });
+  }
+
+  render() {
+    // const transactions = this.state.transactionList.map((t, i) => {
+    //   return (
+    //     <div key={i}>
+    //       <div>{t.type}</div>
+    //     </div>
+    //   );
+    // });
+    return (
+      <div>
+        <VictoryChart>
+          <VictoryBar data={this.state.transactionList} x="type" y="amount" />
+        </VictoryChart>
+      </div>
+    );
+  }
+}
+
+export default App;
