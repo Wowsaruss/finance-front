559c60090ce07a4dd593f25e074ed6d39c48a170
Wowsaruss <russell.hayes.dev@gmail.com>
2019-04-19 07:46:19 -0700

    Graph

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios';
-import { VictoryBar, VictoryChart } from 'victory';
+import Chart from './components/chart';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      transactionList: [{ type: 1, amount: 13000 }]
-    };
-  }
-  componentDidMount() {
-    axios
-      .get(`${process.env.REACT_APP_TRANSACTIONS_API}/transactions`)
-      .then(res => {
-        this.setState({
-          transactionList: res.data
-        });
-      });
-  }
-
   render() {
-    // const transactions = this.state.transactionList.map((t, i) => {
-    //   return (
-    //     <div key={i}>
-    //       <div>{t.type}</div>
-    //     </div>
-    //   );
-    // });
     return (
       <div>
-        <VictoryChart>
-          <VictoryBar data={this.state.transactionList} x="type" y="amount" />
-        </VictoryChart>
+        <Chart />
       </div>
     );
   }
